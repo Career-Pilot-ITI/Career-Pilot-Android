d7f833ed23cee04ce7863b7441924ffa5309b38a
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-07-15 09:37:51 +0300

    feat: Add UIText utility class for handling string resources in Compose

---
+++ app/src/main/java/com/iti/careerpilot/core/util/UIText.kt
@@ -0,0 +1,16 @@
+package com.iti.careerpilot.core.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+
+
+sealed class UIText {
+    class StringResource(val resId: Int, vararg val args: Any) : UIText()
+
+    @Composable
+    fun asString(): String {
+        return when (this) {
+            is StringResource -> stringResource(resId, args)
+        }
+    }
+}
