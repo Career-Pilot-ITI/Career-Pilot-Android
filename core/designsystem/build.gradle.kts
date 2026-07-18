ff7e49c9964f429198cd12e7a9c11fa5daac5c2c
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-07-18 18:50:26 +0300

    build: update material3 and add extended icons dependency.

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -8,6 +8,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.extended.icons)
     implementation(libs.androidx.compose.googlefonts)
     implementation(libs.androidx.compose.material.icons.core)
     implementation(libs.compose.material3)
