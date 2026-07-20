19117e8b789bb50259e880d62e4808baf9ba6a2c
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-07-20 09:27:22 +0300

    feat: add reports use cases in domain layer

---
+++ feature/reports/src/main/java/com/iti/careerpilot/reports/domain/usecase/GetReportDetailsUseCase.kt
@@ -0,0 +1,12 @@
+package com.iti.careerpilot.reports.domain.usecase
+
+import com.iti.careerpilot.reports.domain.repository.ReportsRepository
+import javax.inject.Inject
+
+class GetReportDetailsUseCase @Inject constructor(
+    private val repository: ReportsRepository,
+) {
+    suspend operator fun invoke(
+        sessionId: String,
+    ) = repository.getReportDetails(sessionId)
+}
