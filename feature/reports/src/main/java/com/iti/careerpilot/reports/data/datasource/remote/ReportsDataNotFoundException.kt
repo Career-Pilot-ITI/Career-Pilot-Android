ee1987898fe24e9d0d2078525022dbbdba52c49c
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-07-21 13:58:53 +0300

    feat: implement network remote data source for reports.

--- feature/reports/src/main/java/com/iti/careerpilot/reports/data/datasource/remote/ReportsDataNotFoundException.kt
+++
@@ -1,5 +0,0 @@
-package com.iti.careerpilot.reports.data.datasource.remote
-
-class ReportsDataNotFoundException(
-    sessionId: String,
-) : IllegalArgumentException("No report exists for session id: $sessionId")
