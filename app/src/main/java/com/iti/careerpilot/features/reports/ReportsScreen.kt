ca99b47f51b3bdc74da58c920c233e36efdffd8b
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-07-21 20:49:46 +0300

    refactor: use MaterialTheme background color in Home and Reports screens

--- app/src/main/java/com/iti/careerpilot/features/reports/ReportsScreen.kt
+++ app/src/main/java/com/iti/careerpilot/features/reports/ReportsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -32,7 +33,7 @@ fun ReportsScreen(
 ) {
     Column (
         modifier = Modifier
-            .background(Color.Red)
+            .background(MaterialTheme.colorScheme.background)
             .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
