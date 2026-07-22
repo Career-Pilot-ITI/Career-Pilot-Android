5dcf4046643bd4137a6b714b731673554b992430
Hossam Elgmmal <hossam.elgmmal12@gmail.com>
2026-07-22 16:56:10 +0300

    Feat: add fake remote for practice session

--- feature/practicesession/src/main/java/com/iti/careerpilot/practicesession/domain/di/SessionModule.kt
+++ feature/practicesession/src/main/java/com/iti/careerpilot/practicesession/domain/di/SessionModule.kt
@@ -1,18 +1,11 @@
 package com.iti.careerpilot.practicesession.domain.di
 
-import android.content.Context
-import androidx.media3.exoplayer.ExoPlayer
 import com.iti.careerpilot.practicesession.data.datasource.SessionRepoImpl
-import com.iti.careerpilot.practicesession.data.datasource.remote.SessionRemoteDataSourceImpl
-import com.iti.careerpilot.practicesession.domain.repo.SessionRemoteDataSource
 import com.iti.careerpilot.practicesession.domain.repo.SessionRepo
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -23,8 +16,4 @@ abstract class SessionModule {
         sessionRepoImpl: SessionRepoImpl
     ): SessionRepo
 
-    @Binds
-    abstract fun provideSessionRemoteDataSource(
-        sessionRemoteDataSourceImpl: SessionRemoteDataSourceImpl
-    ): SessionRemoteDataSource
 }
