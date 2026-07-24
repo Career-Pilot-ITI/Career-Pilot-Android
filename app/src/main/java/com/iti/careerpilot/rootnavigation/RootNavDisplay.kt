e2087997bc71135adbb92fb7f316b79f813b4ff4
Hossam Elgmmal <hossam.elgmmal12@gmail.com>
2026-07-24 20:04:57 +0300

    Fix: solve errors

--- app/src/main/java/com/iti/careerpilot/rootnavigation/RootNavDisplay.kt
+++ app/src/main/java/com/iti/careerpilot/rootnavigation/RootNavDisplay.kt
@@ -21,8 +21,6 @@ import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
 import com.iti.careerpilot.core.designsystem.components.CareerPilotAppScaffold
 import com.iti.careerpilot.editprofile.presentation.screen.EditProfileRoot
-import com.iti.careerpilot.features.sessiondetails.SessionDetailsRoot
-import com.iti.careerpilot.features.paywall.PaywallRoot
 import com.iti.careerpilot.features.settings.SettingsRoot
 import com.iti.careerpilot.features.splash.SplashRoot
 import com.iti.careerpilot.login.presentation.login.screen.LoginRoot
