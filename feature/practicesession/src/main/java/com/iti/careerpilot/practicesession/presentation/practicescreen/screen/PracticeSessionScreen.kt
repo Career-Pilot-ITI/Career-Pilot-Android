cd55c06a6697352f7ef6bcac24114eace97db2be
hazemkora660@gmail.com <hazemkora660@gmail.com>
2026-08-13 17:43:21 +0300

    style(practicesession): increase vertical spacing between camera viewport stage and QuestionCard

--- feature/practicesession/src/main/java/com/iti/careerpilot/practicesession/presentation/practicescreen/screen/PracticeSessionScreen.kt
+++ feature/practicesession/src/main/java/com/iti/careerpilot/practicesession/presentation/practicescreen/screen/PracticeSessionScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -355,6 +356,8 @@ fun PracticeSessionScreen(
                         )
                     }
                 }
+
+                Spacer(Modifier.height(12.dp))
 
                 // Question Card (positioned below the camera stage)
                 AnimatedVisibility(
