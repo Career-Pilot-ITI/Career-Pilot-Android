5d6543bc247a5459dde7e4f709ce3840d1fc47af
Hossam Elgmmal <hossam.elgmmal12@gmail.com>
2026-08-13 16:20:24 +0300

    feat: add settings convention plugin

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -1,30 +1,13 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.careerpilot.feature.settings)
 }
 
 android {
     namespace = "com.iti.careerpilot.settings"
-    compileSdk {
-        version = release(37)
-    }
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
 }
 
 dependencies {
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.junit)
+    implementation(project(":core:designsystem"))
+    implementation(project(":core:common"))
+    implementation(project(":core:datastore"))
 }
