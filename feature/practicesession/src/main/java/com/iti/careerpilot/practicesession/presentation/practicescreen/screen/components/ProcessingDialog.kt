8d257013c6200345af501620f84600864d47fb0b
hazemkora660@gmail.com <hazemkora660@gmail.com>
2026-08-13 17:31:24 +0300

    fix(practicesession): remove securePolicy FLAG_SECURE from SessionLoadingDialog and ProcessingDialog

--- feature/practicesession/src/main/java/com/iti/careerpilot/practicesession/presentation/practicescreen/screen/components/ProcessingDialog.kt
+++ feature/practicesession/src/main/java/com/iti/careerpilot/practicesession/presentation/practicescreen/screen/components/ProcessingDialog.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
-import androidx.compose.ui.window.SecureFlagPolicy
 import com.iti.careerpilot.core.designsystem.components.CareerPilotCard
 import com.iti.careerpilot.practicesession.R
 
@@ -39,7 +38,6 @@ fun SessionLoadingDialog() {
             dismissOnBackPress = false,
             dismissOnClickOutside = false,
             usePlatformDefaultWidth = false,
-            securePolicy = SecureFlagPolicy.SecureOn,
         )
     ) {
         CareerPilotCard(
@@ -118,7 +116,6 @@ fun ProcessingDialog(
             dismissOnBackPress = false,
             dismissOnClickOutside = false,
             usePlatformDefaultWidth = false,
-            securePolicy = SecureFlagPolicy.SecureOn,
         )
     ) {
         CareerPilotCard(
