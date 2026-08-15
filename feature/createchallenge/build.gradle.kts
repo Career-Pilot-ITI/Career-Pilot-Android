6421f9530d7110a160b1a82781822bc1eccde250
Hossam Elgmmal <hossam.elgmmal12@gmail.com>
2026-08-16 00:52:30 +0300

    feat: add gradle dependencies

--- feature/createchallenge/build.gradle.kts
+++ feature/createchallenge/build.gradle.kts
@@ -9,4 +9,12 @@ android {
 dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:designsystem"))
+    implementation(project(":core:network"))
+    implementation(project(":core:model"))
+    implementation(project(":core:datastore"))
+    implementation(project(":core:ai"))
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.ai)
 }
