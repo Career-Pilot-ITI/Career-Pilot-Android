75a95ea5d1969cc21743b0027adf7e5e7aa31fc8
Hossam Elgmmal <hossam.elgmmal12@gmail.com>
2026-08-15 20:33:38 +0300

    feat: add challenge dashboard module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,5 @@ dependencies {
     implementation(project(":feature:ats"))
     implementation(project(":feature:challenges"))
     implementation(project(":feature:createchallenge"))
+    implementation(project(":feature:challengedashboard"))
 }
